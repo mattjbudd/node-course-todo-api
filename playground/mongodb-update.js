73cae0e840ab5ec0912c1d523a0c242a6e64459c
BuildTools <unconfigured@null.spigotmc.org>
2018-12-05 15:52:00 -0700

    add update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5c085102ad7e7204e2fe07eb')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5c0846b8e704507e38597ec3')
+  }, {
+    $set: {
+      name: 'Matt Budd'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
